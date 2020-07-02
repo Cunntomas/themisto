f26e9bae8f5c6984e58453230824ad141cc2f4a3
Tomas <cunn.tomas@gmail.com>
2020-07-02 14:03:33 -0300

    fix port

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 require('dotenv').config();
 const logger = require('./lib/logger');
 const app = require('./app');
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.SERVER_PORT || 8000;
 
 const application = app.initialize();
-application.listen(process.env.SERVER_PORT);
-logger.info(`Your server is listening on port ${process.env.SERVER_PORT}`);
+application.listen(PORT);
+logger.info(`Your server is listening on port ${PORT}`);
