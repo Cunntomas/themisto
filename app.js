134ffbfd1eab9a5cad5a8f917220cc6f8a73c62c
Tomas <cunn.tomas@gmail.com>
2020-07-02 20:35:11 -0300

    explanatory comments

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const logger = require('./lib/logger');
 
 function initialize() {
     app.use(bodyParser.json());
+
+    //ROUTES ARE CREATED USING THE NAME OF THE FILE AND THE NAME OF THE ROUTE WITH THE FORMAT
+    // /api/name_of_route_file/name_of_route
     Object.keys(routes).forEach((key) => {
         app.use(`/api/${key}`, routes[key]);
     });
-
+    
     app.use(function(req, res, next) {
         let err = new Error('Not Found');
         err.status = 404;
